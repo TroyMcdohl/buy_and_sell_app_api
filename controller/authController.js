d61f4f3f3fd104a4db3950e7867a68aacc1654a6
troymcdohl <troymcdohl@gmail.com>
2024-02-02 18:56:41 +0630

    http

--- controller/authController.js
+++ controller/authController.js
@@ -58,7 +58,6 @@ exports.login = catchAsync(async (req, res, next) => {
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRECT);
 
   res.cookie("jwt", token, {
-    httpOnly: true,
     expires: new Date(Date.now() + 24 * 3600 * 1000),
 
     sameSite: "none",
