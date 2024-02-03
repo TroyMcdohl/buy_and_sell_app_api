3e732adeaa43050db697e6ba32e1377477d68caf
troymcdohl <troymcdohl@gmail.com>
2024-02-03 11:51:55 +0630

    tet

--- controller/authController.js
+++ controller/authController.js
@@ -62,6 +62,7 @@ exports.login = catchAsync(async (req, res, next) => {
     httpOnly: true,
     secure: true,
     sameSite: "none",
+    domain: "buy-and-sell-app.vercel.app",
   });
 
   res.status(200).json({
