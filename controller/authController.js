9e4406a73120c27a5f64225c60706b42c2b2008f
troymcdohl <troymcdohl@gmail.com>
2024-02-03 13:19:12 +0630

    tet

--- controller/authController.js
+++ controller/authController.js
@@ -170,6 +170,8 @@ exports.logout = (req, res) => {
   res.cookie("jwt", "logout", {
     expires: new Date(Date.now() + 5 * 1000),
     httpOnly: true,
+    sameSite: "none",
+    secure: true,
   });
 
   res.status(200).json({ status: "success" });
